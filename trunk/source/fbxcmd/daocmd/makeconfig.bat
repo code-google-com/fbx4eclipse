a3e60e78423b92ac9025e655e1e2a04096ce0ec5
tazpn314 <tazpn314@ccf8930c-dfc1-11de-9043-17b7bd24f792>
2009-12-03 04:44:59 +0000

    fbxcmd V1.2

--- trunk/source/fbxcmd/daocmd/makeconfig.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Quick script to create config.h
-REM IF EXIST "%PROGRAMFILES%\TortoiseSVN\bin\SubWCRev.exe" (
-REM    "%PROGRAMFILES%\TortoiseSVN\bin\SubWCRev.exe" . Core\config.h.in Core\config.h > nul 2<&1
-REM )
